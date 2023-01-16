e41fe253ee63e18d2cd26135c6a9c34761d90899
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-01-16 10:31:40 -0300

    Deve alterar conta com sucesso

--- src/main/java/br/com/fernando/tests/BarrigaTest.java
+++ src/main/java/br/com/fernando/tests/BarrigaTest.java
@@ -1,5 +1,6 @@
 package br.com.fernando.tests;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.fernando.rest.core.BaseTest;
@@ -12,6 +13,23 @@ import java.util.Map;
 
 public class BarrigaTest extends BaseTest{
 	
+	private String TOKEN;
+	
+	@Before
+	public void login () {
+		Map<String, String> login = new HashMap<>();
+		login.put("email", "fernandooo@gmail.com");
+		login.put("senha", "Senha");
+		
+		TOKEN = given()
+		.body(login)
+		.when()
+		.post("/signin")
+		.then()
+		.statusCode(200)
+		.extract().path("token");
+	}
+	
 	@Test
 	public void naoDeveAcessarAPISemToken() {
 		given()
@@ -23,25 +41,26 @@ public class BarrigaTest extends BaseTest{
 	
 	@Test
 	public void deveIncluirContaComSucesso() {
-		Map<String, String> login = new HashMap<>();
-		login.put("email", "fernandooo@gmail.com");
-		login.put("senha", "Senha");
-		
-		String token = given()
-		.body(login)
-		.when()
-		.post("/signin")
-		.then()
-		.statusCode(200)
-		.extract().path("token");
-		
 		given()
-		.header("Authorization", "JWT " + token)
+		.header("Authorization", "JWT " + TOKEN)
 		.body("{\"nome\": \"conta qualquer\"}")
 		.when()
 		.post("/contas")
 		.then()
 		.statusCode(201);
+	}
+	
+	@Test
+	public void deveAlterarContaComSucesso() {
+		given()
+		.header("Authorization", "JWT " + TOKEN)
+		.body("{\"nome\": \"conta alterada\"}")
+		.when()
+		.put("/contas/1557472")
+		.then()
+		.log().all()
+		.statusCode(200)
+		.body("nome", is("conta alterada"));
 	}
 	
 }
